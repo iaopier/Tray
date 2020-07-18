85198a77982cda8b3751a7946c2925161f4c5927
Piercarlo <pierhett@gmail.com>
2020-07-18 14:59:52 -0300

    Método criar vendedor funcionando. Necessita teste.

--- TrayAPI/src/main/java/com/trayapi/vendedores/Service/impl/VendedorServiceImpl.java
+++ TrayAPI/src/main/java/com/trayapi/vendedores/Service/impl/VendedorServiceImpl.java
@@ -1,11 +1,13 @@
 package com.trayapi.vendedores.Service.impl;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.trayapi.vendedores.Models.VendedorModel;
 import com.trayapi.vendedores.Repository.VendedorRepository;
 import com.trayapi.vendedores.Service.VendedorService;
 
+@Service
 public class VendedorServiceImpl implements VendedorService {
 
 	@Autowired
@@ -14,7 +16,7 @@ public class VendedorServiceImpl implements VendedorService {
 	@Override
 	public VendedorModel save(VendedorModel vendedor) {
 		vendedorRepository.save(vendedor);
-		return null;
+		return vendedor;
 	}
 
 }
